b4c9b3b1459ff163e0b8853678322c2a2a455a50
Bruno Silvam <bruno.pfsilva@hotmail.com>
2021-11-02 13:22:24 +0000

    add radon cactus show

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let isJump = false;
 
 console.log(dino);
+console.log(background);
 
 document.addEventListener('keyup',(event)=>{
     if(event.keyCode === 32)
-    jump();
+    if(!isJump){
+        jump();
+    }
 });
 
 function jump (){
     let position = 0;
     let UpInterval = setInterval(()=>{
-
+        isJump = true
         if(position >=150){
             clearInterval(UpInterval);
             let downInterval = setInterval (()=>{
                 if(position <= 0 ){
                 clearInterval(downInterval);
+                isJump = false
                 }else {
                     position -= 20;
                     dino.style.bottom = position + `px`;    
@@ -24,7 +29,8 @@ function jump (){
                
             },20) 
         }else {
-            position += 20;
+            position += 19
+            20;
             dino.style.bottom = position + `px`;
         }
 
@@ -32,3 +38,32 @@ function jump (){
 
     }, 20)
 }
+
+
+function createCactus (){
+    const cactus = document.createElement('div');
+    let positionCactus = 1000;
+
+
+    let cactusRandon = Math.random() * 6000;
+
+    cactus.classList.add('cactus')
+    cactus.style.left = positionCactus +  `px`
+    background.appendChild(cactus)
+
+
+    let leftInterval = setInterval(() => {
+      
+        if(positionCactus < 60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else {
+            positionCactus -= 10;
+            cactus.style.left = positionCactus + `px`;
+        }
+    }, 20);
+
+    setTimeout( createCactus , cactusRandon);
+}
+
+createCactus();     
